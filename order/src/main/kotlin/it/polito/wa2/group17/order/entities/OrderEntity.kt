6da216b12044eb4f36123977533f04ac386d201b
Lorenzo <lorenzoappendini@gmail.com>
2021-07-27 17:37:25 +0200

    db settings + entities + repo

---
+++ order/src/main/kotlin/it/polito/wa2/group17/order/entities/OrderEntity.kt
@@ -0,0 +1,19 @@
+package it.polito.wa2.group17.order.entities
+
+import it.polito.wa2.group17.common.utils.BaseEntity
+import org.jetbrains.annotations.NotNull
+import javax.persistence.Entity
+import javax.persistence.OneToMany
+import javax.persistence.Table
+import javax.validation.constraints.Min
+
+@Entity
+class OrderEntity(
+    @NotNull
+    var buyer: String,
+    @OneToMany(mappedBy = "order")
+    var productOrders: MutableList<ProductOrderEntity>,
+    @NotNull
+    @Min(0)
+    var price: Long,
+) : BaseEntity<Long>()
